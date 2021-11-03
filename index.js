6d14beaf3cd7a5084a017cb0b2af1ea3975f09a7
axbg <bisagalexstefan@gmail.com>
2021-11-03 22:44:33 +0200

    Refactored code

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 const axios = require('axios');
 const express = require('express');
 const cors = require('cors');
-const { PORT, configCheck } = require('./constants');
+
+require('dotenv').config()
 
 const app = express();
 
 app.use(cors());
 
 app.get("/", async (req, res) => {
-
 	if (req.query.url) {
 		const response = await axios.get(req.query.url);
 		return res.status(200).send({ response: response.data });
@@ -17,5 +17,5 @@ app.get("/", async (req, res) => {
 	return res.status(400).send({ message: "URL param is empty" });
 });
 
-configCheck();
-app.listen(PORT, () => console.log("app running on localhost:" + 8080));
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => console.log("app running on http://localhost:" + PORT));
